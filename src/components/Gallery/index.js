58a340f53eabba226472d7485893c40f345c6334
nanoky <konancharlesyao@gmail.com>
2022-04-19 09:26:39 +0000

    gallery title skeleton added

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,4 +1,5 @@
 import { Container, Stack } from "react-bootstrap"
+import Skeleton from "react-loading-skeleton";
 import { ImageGrid } from "../ImagesGrid";
 
 
@@ -6,9 +7,24 @@ export const Gallery = ({images, title, dataLoading}) => {
     return (
         <Container className="mb-5">
             <Container className="mb-5">
-                <h3 className="text-center">{title ? title : "Gallery"}</h3>
+                <GalleryTitle title={title} />
             </Container>
             <ImageGrid data={images} dataLoading={dataLoading} />
         </Container>
+    )
+}
+
+const GalleryTitle = ({title}) => {
+
+    return (
+        <h3 className="text-center">
+        {
+            title ? (
+                <span>{title}</span>
+            ) : (
+                <Skeleton />
+            )
+        }
+        </h3>
     )
 }
