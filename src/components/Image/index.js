f0ac442839240127059ac4cb4a01741d55135b5b
nanoky <konancharlesyao@gmail.com>
2022-04-19 08:33:09 +0000

    added a skeleton loader

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import { Container, Image } from "react-bootstrap";
+import Skeleton from "react-loading-skeleton";
+import SkeletonTheme from "react-loading-skeleton/dist"
+import { log } from "../../helpers/logger";
+import 'react-loading-skeleton/dist/skeleton.css'
 
 
-export const GalleryImage = ({source}) => {
+export const GalleryImage = ({ source, onLoad, ...props }) => {
 
+    /* return (loaded) ? (
+        <Image src={source} fluid onLoad={imageLoaded} />
+    ) : (
+        <SkeletonTheme width="100%" height="150px">
+            <Skeleton />
+        </SkeletonTheme>
+
+    )
+        ; */
     return (
-        <Image src={source} fluid />
-    );
+        <Image src={source} fluid {...props} onLoad={() => {
+            onLoad(source);
+        }} />
+    )
 }
--- src/components/ImagesGrid/index.js
+++ src/components/ImagesGrid/index.js
@@ -1,4 +1,8 @@
+import { useState } from "react"
 import { Col, Container, Row } from "react-bootstrap"
+import Skeleton from "react-loading-skeleton";
+import SkeletonTheme from "react-loading-skeleton/dist"
+import { log } from "../../helpers/logger";
 import { GalleryImage } from "../Image"
 import { Loader } from "../loader"
 import { ItemsPagination } from "../Pagination"
@@ -6,11 +10,33 @@ import { ItemsPagination } from "../Pagination"
 export const ImageGrid = ({ data, dataLoading }) => {
     const nb_item_row = 5
     const nb_item_page = 15;
+
+    const [loaded, setLoaded] = useState({});
+
+    var nb = 0;
+
+    const imagesLoaded = (id) => {
+        setLoaded({
+            [id] : true,
+            ...loaded
+        });
+        log(Object.keys(loaded).length);
+    }
     return (
         <Container>
             {
                 (!dataLoading) ? (
-                    <ItemsPagination items={data} nb_item_page={nb_item_page} ItemComponent={ImageItemPagination} nb_item_row={nb_item_row} />
+                    <Container>
+                        <Container className={(Object.keys(loaded).length >= nb_item_page) ? "visually-hidden" : ""}>
+                            <ItemsPagination items={data} nb_item_page={nb_item_page} ItemComponent={SkeletonItemPagination} nb_item_row={nb_item_row} />
+                        </Container>
+
+                        <Container className={(Object.keys(loaded).length < nb_item_page) ? "visually-hidden" : ""}>
+                            <ItemsPagination items={data} nb_item_page={nb_item_page} ItemComponent={ImageItemPagination} nb_item_row={nb_item_row} onLoad={imagesLoaded} />
+                        </Container>
+
+                    </Container>
+
                 ) : (
                     <Loader />
                 )
@@ -19,9 +45,18 @@ export const ImageGrid = ({ data, dataLoading }) => {
     )
 }
 
-const ImageItemPagination = ({item, ...props}) => {
+const ImageItemPagination = ({ item, ...props }) => {
 
     return (
         <GalleryImage source={item.src} {...props} />
+    );
+}
+
+const SkeletonItemPagination = ({ item }) => {
+
+    return (
+        <SkeletonTheme width="100%" height="150px">
+            <Skeleton />
+        </SkeletonTheme>
     );
 }
